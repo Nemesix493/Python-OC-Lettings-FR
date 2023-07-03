d871f663cce024fc6f4f5f86e4479c0339bc39b8
Nemesix493 <verquerre.kevin@gmail.com>
2023-07-03 18:16:20 +0200

    refactor : create base_site app to replace oc_letting_site app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
-from lettings import urls as lettings_urls
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include('base_site.urls', namespace='base_site')),
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),

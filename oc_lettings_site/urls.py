53f800ed9107b5868d051f7bc0418c2c43e0d19e
Nemesix493 <verquerre.kevin@gmail.com>
2023-07-03 17:53:43 +0200

    refactor : move profiles views in profiles app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -7,7 +7,6 @@ from lettings import urls as lettings_urls
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls', namespace='lettings')),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
 ]

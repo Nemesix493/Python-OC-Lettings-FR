e84703e830e909650763a8fd724722e4e8dfb690
Nemesix493 <verquerre.kevin@gmail.com>
2023-09-22 01:10:52 +0200

    feat : set up the production settings

---
+++ oc_lettings_site/settings/production.py
@@ -0,0 +1,21 @@
+import os
+
+import dj_database_url
+
+from .base import *  # noqa: F401, F403
+
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+)
+
+ALLOWED_HOSTS = ["*"]
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {
+    'default': db_from_env
+}
